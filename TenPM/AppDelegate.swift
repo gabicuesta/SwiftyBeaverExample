ac8ebc6e0e7b75b6e6a80f8726a60db02b58c52d
Gabriel Cuesta Arza <gabriel.cuesta.contractor@bbva.com>
2017-04-18 09:39:01 +0200

    First version. It needs that you install the app Swifty Beaver in your mac

---
+++ TenPM/AppDelegate.swift
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import SwiftyBeaver
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    setupSwiftyBeaverLogging()
+    SwiftyBeaver.info("Hello SwiftyBeaver Logging!")
+    return true
+  }
+  
+  func setupSwiftyBeaverLogging() {
+    let console = ConsoleDestination()
+    SwiftyBeaver.addDestination(console)
+    
+    let platform = SBPlatformDestination(appID: "Z5RMeO",
+                                         appSecret: "0njpXo7rhij64gmPzdguvd9XecjzbhTa",
+                                         encryptionKey: "jviaqvvtdrkafhtaknsCrnbJ7g7ik56m")
+    
+    SwiftyBeaver.addDestination(platform)
+    
+  }
+}
+
